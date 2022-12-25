28c9adf391e92cb2ddb821f9b227c71734c60047
kikerkov <kikerkov@gmail.com>
2022-12-25 02:48:20 +0100

    Trying new Input System

--- Escape_Runner/Assets/Scripts/Player.cs
+++ Escape_Runner/Assets/Scripts/Player.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public UnityEvent OnPlayerInteract;
-    [SerializeField] private GameObject _playerPrefab;
-    [SerializeField] private GameObject _playerContainer;
-    public Vector2 PlayerPosition;
     public float PlayerSpeed;
+    public Vector2 PlayerPosition = Vector2.zero;
+    private Rigidbody2D _playerRigidBody;
+
     private Player_Camera _playerCamera;
 
     void Start()
     {
-        PlayerSpeed = 0.15f;
+        _playerRigidBody = GetComponent<Rigidbody2D>();
+        PlayerSpeed = 1f;
         _playerCamera = GameObject.Find("Player_Camera").GetComponent<Player_Camera>();
         if (_playerCamera == null){Debug.LogWarning("_playerCamera is NULL");}
         _playerCamera.transform.parent = this.transform;
@@ -22,25 +23,11 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        PlayerMovement();
+        _playerRigidBody.velocity = PlayerPosition * PlayerSpeed;
     }
 
-    private void PlayerMovement()
+    private void PlayerMovement(InputValue value)
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector2 inputVector = new Vector2(horizontal, vertical);
-
-        if (horizontal != 0)
-        {
-            transform.Translate(inputVector * PlayerSpeed);
-
-            Debug.Log("Horizontal input");
-        }
-        if (vertical != 0)
-        {
-            transform.Translate(inputVector * PlayerSpeed);
-            Debug.Log("Vertical input");
-        }
+        PlayerPosition = value.Get<Vector2>();
     }
 }
